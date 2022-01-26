24daac4119f741649e8f607299fd766043383222
Shashank <shashankmj@nousinfo.com>
2022-01-26 06:17:58 +0530

    kick starters working finally

--- src/App.js
+++ src/App.js
@@ -1,150 +1,88 @@
 import React, {useEffect, useState} from "react";
 import styled from "styled-components";
 import axios from 'axios';
-import {createWorker} from "tesseract.js";
-
-const worker = createWorker({
-  logger: (m) => console.log(m),
-});
-
-const now = Date.now();
 
 export default function Home() {
 
   const [projects, setProjects] = useState([]);
-  const [mxPrice, setMxPrice] = useState(2);
-  const [amount, setAmount] = useState(72333);
+  const [loading, setLoading] = useState(true);
+  const [mxPrice, setMxPrice] = useState(localStorage.getItem("MX-PRICE") ?? 1.76);
+  const [amount, setAmount] = useState(localStorage.getItem("AMOUNT") ?? 75000);
 
   const fetchProjects = async () => {
-    try {
-      const res = await axios.get("https://api.paybook.club:8081/mexc/kick-starters");
-      const activeProjects = res.data.data.filter((item, i) => item.endTime > now);
-      const items = [];
-      const ExtractPrice = async ({webProjectIntroductionEn, profitCurrency, profitCurrencyFullName, profitCurrencyIcon, currentVoteQuantity, totalSupply}) => {
-
-        const key = `${profitCurrency}+${profitCurrencyFullName}`;
-        let price = localStorage.getItem(key);
-        if (!price) {
-          // const base64String = await getBase64ImageFromUrl(`https://www.mexc.com/api/file/download/${webProjectIntroductionEn}`);
-          const text = await ExtractTextFromImage(`https://www.mexc.com/api/file/download/${webProjectIntroductionEn}`);
-          const result = text.match(/\$(.*)?\)/);
-          price = result[1];
-          localStorage.setItem(key, price);
-        }
-
-        items.push({
-          name: profitCurrencyFullName,
-          currency: profitCurrency,
-          icon: `https://www.mexc.com//api/file/download/${profitCurrencyIcon}`,
-          votes: currentVoteQuantity ?? 500,
-          totalReward: totalSupply,
-          price: price ?? 0
-        });
-      };
-
-      const promises = activeProjects.map(p => ExtractPrice(p));
-
-      Promise.all(promises).then(() => {
-        setProjects(items);
-      }).catch(e => console.log('Error:' + e));
-    } catch (err) {
-      console.log(err);
-    }
-  };
-
-
-  const getBase64ImageFromUrl = async function (imageUrl) {
-    const res = await fetch(imageUrl);
-    const blob = await res.blob();
-
-    return new Promise((resolve, reject) => {
-      const reader = new FileReader();
-      reader.addEventListener("load", function () {
-        resolve(reader.result);
-      }, false);
-
-      reader.onerror = () => {
-        return reject(this);
-      };
-      reader.readAsDataURL(blob);
-    })
-  };
-
-  const ExtractTextFromImage = async (ImageString) => {
-    await worker.load();
-    await worker.loadLanguage("eng");
-    await worker.initialize("eng");
-    const {
-      data: {
-        text
-      },
-    } = await worker.recognize(ImageString);
-    await worker.terminate();
-    return text;
+    const res = await axios.get("https://api.paybook.club:8085/mexc/kick-starters?count=10");
+    setProjects(res.data);
+    setLoading(false);
   };
 
   useEffect(() => {
     fetchProjects();
-    setMxPrice(localStorage.getItem("MX-PRICE") ?? 2);
-    setAmount(localStorage.getItem("AMOUNT") ?? 119343.85);
-    // const interval = setInterval(fetchProjects, 10000);
-    // return () => clearInterval(interval);
   }, []);
 
   return <Container>
-    <Greeting>Hello Shashank, Here are today&apos;s kick-starters</Greeting>
-    {projects.map(({name, currency, icon, votes, totalReward, price, image}, index) =>
-      <Row key={currency}>
-        <Icon src={icon} alt={name} />
-        <Column>
-          <Title>{name}</Title>
-          <Caption>{`[${currency}]`}</Caption>
-          <Label>{votes}</Label>
-        </Column>
-        <Column>
-          <Label>Price</Label>
-          <Input value={price} onChange={(e) => {
-            const clonedProjects = [...projects];
-            clonedProjects[index].price = e.target.value;
-            setProjects(clonedProjects)
-          }} />
-        </Column>
-        <Column>
-          <Label>Reward</Label>
-          <Title>{`₹${((totalReward * price) / (mxPrice * votes)) * amount}`}</Title>
-        </Column>
+    <KickStarters>
+      <Header>kick-starters</Header>
+
+      {loading
+        ? <Title>Loading... Please wait</Title>
+        : projects.map(({name, currency, icon, votes, totalReward, price}) =>
+          <Row key={currency}>
+            <Icon src={icon} alt={name} />
+            <Column>
+              <Title>{name}</Title>
+              <Caption>{`[${currency}]`}</Caption>
+              <Label>{`$${price}`}</Label>
+            </Column>
+            <Column>
+              <Label>Reward</Label>
+              <Title>{`₹${((totalReward * price) / (mxPrice * votes)) * amount}`}</Title>
+            </Column>
+          </Row>
+        )}
+
+      <Row>
+        <Label>Mx Price</Label>
+        <Input value={mxPrice} onChange={(e) => setMxPrice(e.target.value)} />
       </Row>
-    )}
 
-    <Row>
-      <Label>Mx Price</Label>
-      <Input value={mxPrice} onChange={(e) => setMxPrice(e.target.value)} />
-    </Row>
+      <Row>
+        <Label>Investment</Label>
+        <Input value={amount} onChange={(e) => setAmount(e.target.value)} />
+      </Row>
 
-    <Row>
-      <Label>Investment</Label>
-      <Input value={amount}  onChange={(e) => setAmount(e.target.value)}/>
-    </Row>
-
-    <Button onClick={() => {
-      projects.forEach(p => localStorage.setItem(p.currency, p.price));
-      localStorage.setItem("MX-PRICE", mxPrice);
-      localStorage.setItem("AMOUNT", amount);
-    }}>
-      Save Prices
-    </Button>
+      <Button onClick={() => {
+        localStorage.setItem("MX-PRICE", mxPrice);
+        localStorage.setItem("AMOUNT", amount);
+      }}>
+        Save Prices
+      </Button>
+    </KickStarters>
+    <Divider/>
+    <Defis>
+      <Header>DeFis</Header>
+    </Defis>
   </Container>
 }
 
 const Container = styled.div`
+  display: flex;
+  flex-direction: row;
   padding: 15px;
-  border: 2px solid #81C2C0;
-  margin: 15px auto;
-  border-radius: 10px;
-  width: 1000px;
 `;
 
-const Greeting = styled.div`
+const KickStarters = styled.div`
+  flex:1;
+`;
+
+const Defis = styled.div`
+  flex:1;
+`;
+
+const Divider=styled.div`
+border-left: 1px solid gainsboro;
+`;
+
+const Header = styled.div`
   font-family: "Manrope",sans-serif;
   font-style: normal;
   font-weight: 600;
